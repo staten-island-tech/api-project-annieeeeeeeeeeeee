7bbee24c3d712fbfb1788dac0be00bdd27a6d7f8
annieeeeeeeeeeeee <annieou000@gmail.com>
2023-01-04 10:16:29 -0500

    1

---
+++ vite-project/main.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+const URL = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error();
+    } else {
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("app").textContent = data.setup;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
