acdccf1efdcc39458fe36825f545f0c5c7fa93eb
annieeeeeeeeeeeee <annieou000@gmail.com>
2023-01-06 10:16:30 -0500

    3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,9 @@
-import "./style.css";
-import { DOMSelectors } from "./dom";
+//import "..styles/style.css";
+//import { DOMSelectors } from "./dom";
 
 const URL = "https://v2.jokeapi.dev/joke/Any?safe-mode";
 
-async function getData(URL) {
+/* async function getData(URL) {
   try {
     const response = await fetch(URL);
     if (response.status < 200 || response.status > 299) {
@@ -17,5 +17,39 @@ async function getData(URL) {
   } catch (error) {
     console.log(error);
   }
+} */
+//getData(URL);
+
+async function filter(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error();
+    } else {
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      const result = data.array.filter((e) => e.category === "Misc");
+
+      console.log(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-getData(URL);
+
+filter(URL);
+
+const category = "https://v2.jokeapi.dev/categories";
+/* 
+async function getCategory(category){
+  try {
+    const reponse = await fetch (category);
+    if (reponse.status <200 || reponse.status >299) {
+      throw new Error();
+    } else {
+      const data = await reponse.json(); 
+
+    }
+  }
+}; */
